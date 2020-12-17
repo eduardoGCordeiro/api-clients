19993c01894f3425806fc25f7665af2472ec8b5a
eduardoGCordeiro <eduardoguilhermecordeiro@gmail.com>
2020-12-17 12:45:01 -0300

    Adicionando as rotas para manipulação de clientes.

--- src/routes/Cliente/index.js
+++ src/routes/Cliente/index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const ClienteController = require('../../controllers/ClienteController');
 const routes = express.Router();
 
-routes.get('/cliente', (req, res) => {
+routes.get('/clientes', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.get('/clientes/:codigo_cliente', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.post('/clientes', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.put('/clientes/:codigo_cliente', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.delete('/clientes/:codigo_cliente', (req, res) => {
     return res.json({ eduardo: 'teste' });
 });
 
