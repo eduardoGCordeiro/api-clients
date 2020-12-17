e60992331aeddb68a88314a56c84daaac6eecb42
eduardoGCordeiro <eduardoguilhermecordeiro@gmail.com>
2020-12-17 13:04:15 -0300

    Adicionando as rotas para manipulação de pedidos.

--- src/routes/Pedido/index.js
+++ src/routes/Pedido/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const PedidoController = require('../../controllers/ClienteController');
 const routes = express.Router();
 
-routes.get('/pedido', (req, res) => {
-    return res.json({ eduardo: 'pedido' });
+routes.get('/pedidos', (req, res) => {
+    return res.json({ eduardo: 'teste' });
 });
+
+routes.get('/pedidos/:codigo_pedido', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.post('/pedidos', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.put('/pedidos/:codigo_pedido', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.delete('/pedidos/:codigo_pedido', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
 
 module.exports = routes;
