b556bbf3eb4d5128a63dec0ea21d404c2df246e9
eduardoGCordeiro <eduardoguilhermecordeiro@gmail.com>
2020-12-17 13:02:16 -0300

    Adicionando as rotas para manipulação de produtos.

--- src/routes/Produto/index.js
+++ src/routes/Produto/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 const ProdutoController = require('../../controllers/ClienteController');
 const routes = express.Router();
 
-routes.get('/produto', (req, res) => {
-    return res.json({ eduardo: 'produto' });
+routes.get('/produtos', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.get('/produtos/:codigo_cliente', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.post('/produtos', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.put('/produtos/:codigo_cliente', (req, res) => {
+    return res.json({ eduardo: 'teste' });
+});
+
+routes.delete('/produtos/:codigo_cliente', (req, res) => {
+    return res.json({ eduardo: 'teste' });
 });
 
 module.exports = routes;
