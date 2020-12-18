5bcb782e869c2e4ebe07f9feddb87ca16ca32c83
eduardoGCordeiro <eduardoguilhermecordeiro@gmail.com>
2020-12-18 13:14:31 -0300

    Adicionando as informações do pedido para o envio de email e geração de relatório.

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -1,6 +1,43 @@
+const sequelize = require('sequelize');
 const Pedido = require('../models/Pedido');
 const Cliente = require('../models/Cliente');
-const Produto = require('../models/Produto');
+
+const informacoesPedido = async (codigo_pedido) => {
+    const pedido = await Pedido.findByPk(codigo_pedido, {
+        where: {
+            codigo_pedido
+        },
+        attributes: [
+            'codigo_pedido',
+            'data_do_pedido',
+            'forma_de_pagamento',
+            'observacao'
+        ],
+        include: [
+            {
+                association: 'cliente'
+            },
+            {
+                association: 'produtos',
+                attributes: [
+                    'codigo_produto',
+                    'nome',
+                    'cor',
+                    'tamanho',
+                    'valor',
+                    [sequelize.literal('produtos.valor * quantidade'), 'valor_total_produto']
+                ]
+            }
+        ]
+    });
+
+    //Adicionando o total do pedido nas informações do pedido.
+    let total_pedido = 0;
+    pedido.produtos.map(produto => total_pedido += produto.dataValues.valor_total_produto);
+    pedido.dataValues.valor_total_pedido = total_pedido;
+
+    return pedido;
+};
 
 module.exports = {
     async listar(req, res) {
@@ -57,6 +94,22 @@ module.exports = {
                 codigo_pedido: codigo_pedido
             }
         });
+
+        return res.json(pedido);
+    },
+
+    async report(req, res) {
+        const { codigo_pedido } = req.params;
+
+        const pedido = await informacoesPedido(codigo_pedido);
+
+        return res.json(pedido);
+    },
+
+    async sendEmail(req, res) {
+        const { codigo_pedido } = req.params;
+
+        const pedido = await informacoesPedido(codigo_pedido);
 
         return res.json(pedido);
     },
--- src/routes/Pedido.js
+++ src/routes/Pedido.js
@@ -5,6 +5,8 @@ const routes = express.Router();
 routes.get('/pedidos', PedidoController.listar);
 routes.get('/pedidos/:codigo_pedido', PedidoController.detalhes);
 routes.post('/pedidos', PedidoController.salvar);
+routes.post('/pedidos/:codigo_pedido/report', PedidoController.report);
+routes.post('/pedidos/:codigo_pedido/sendemail', PedidoController.sendEmail);
 routes.put('/pedidos/:codigo_pedido', PedidoController.atualizar);
 routes.delete('/pedidos/:codigo_pedido', PedidoController.deletar);
 
